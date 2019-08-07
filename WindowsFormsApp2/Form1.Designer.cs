2d9340d6a9a52f565f2c23f7cdf63971f18cbb8c
Adam Sives <60190319@healthtech.nswhealth.net>
2019-08-07 14:54:43 +1000

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectHL7File = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectHL7File
+            // 
+            this.btnSelectHL7File.Location = new System.Drawing.Point(196, 83);
+            this.btnSelectHL7File.Name = "btnSelectHL7File";
+            this.btnSelectHL7File.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectHL7File.TabIndex = 0;
+            this.btnSelectHL7File.Text = "Select .hl7 file";
+            this.btnSelectHL7File.UseVisualStyleBackColor = true;
+            this.btnSelectHL7File.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSelectHL7File);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectHL7File;
+    }
+}
+
---
+++ WindowsFormsApp2/ValidatedPID.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class ValidatedPID
+    {
+        string[] segmentFields = specificSegment.Split('|');
+        //TODO-------handle ~ multiple field instances
+        string[] IDFields = segmentFields[3].Split('^');
+
+                                try
+                                {
+                                    if (getCDMData(specificSegment.Substring(0, 3), "3", "4").Contains(IDFields[3]) == false)
+
+                                    {
+                                        MessageBox.Show("yes" + IDFields[3]);//-------------debug
+                                        Console.WriteLine("Message Control ID:");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("no:" + IDFields[3]);//-------------debug
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show("no PID-3.4");
+                                }
+    }
+}
